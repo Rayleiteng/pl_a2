2dfa7a36e48cb75e13df3e0a529699b4bf885cd9
35007 <lei24627@gtiit.edu.cn>
2026-05-28 15:53:56 +0800

    first commit

--- sudoku.pl
+++ sudoku.pl
@@ -20,7 +20,7 @@ sudoku2([
   [_, 5, _, _, _, 3, _, 2, 8],
   [_, _, 9, 3, _, _, _, 7, 4],
   [_, 4, _, _, 5, _, _, 3, 6],
-  [7, _, 3, _, 1, 8, _, _, _],
+  [7, _, 3, _, 1, 8, _, _, _]
 ]).
 
 %% Predicate to show a Sudoku in the screen.
@@ -30,7 +30,7 @@ show([X0, X1, X2, X3, X4, X5, X6, X7, X8]) :-
   write("+---+---+---+\n"),
   showRow(X3), showRow(X4), showRow(X5),
   write("+---+---+---+\n"),
-  showRow(X7), showRow(X8), showRow(X9),
+  showRow(X6), showRow(X7), showRow(X8),
   write("+---+---+---+\n\n\n").
 showRow([X0, X1, X2, X3, X4, X5, X6, X7, X8]) :-
   write("|"),
